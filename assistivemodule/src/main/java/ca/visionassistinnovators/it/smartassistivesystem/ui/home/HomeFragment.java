ec30f1482274f29f8b9281441a2208f9c433f3b4
PatelKush7387 <kush26540@gmail.com>
2025-09-21 16:14:08 -0400

    Created HomeFragment.java and added code

---
+++ assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/home/HomeFragment.java
@@ -0,0 +1,38 @@
+/*
+ * Full Name: Kush Patel
+ * Student ID: N01657387
+ * Section: CENG 258
+ * Team Members:
+ * Sarang Prajapati – N01662036
+ * Krish Patel – N01666556
+ * Daksh Rana – N01664095
+ */
+
+package ca.visionassistinnovators.it.smartassistivesystem.ui.home;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate your Home fragment UI
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
