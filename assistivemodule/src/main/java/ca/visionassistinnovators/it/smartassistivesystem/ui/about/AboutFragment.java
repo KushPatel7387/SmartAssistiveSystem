525aa5f04ca5597f2032bee8153e4b37096fc92f
PatelKush7387 <kush26540@gmail.com>
2025-09-21 22:09:41 -0400

    updated manifest file

---
+++ assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/about/AboutFragment.java
@@ -0,0 +1,36 @@
+package ca.visionassistinnovators.it.smartassistivesystem.ui.about;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import ca.visionassistinnovators.it.smartassistivesystem.R;
+
+/**
+ * AboutFragment
+ * Shows project details, team names, IDs, and section number.
+ *
+ * Team Members:
+ * - Sarang Prajapati – N01662036
+ * - Krish Patel – N01666556
+ * - Kush Patel – N01657387
+ * - Daksh Rana – N01664095
+ * Section: 3DTues
+ */
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+        // Required empty constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
