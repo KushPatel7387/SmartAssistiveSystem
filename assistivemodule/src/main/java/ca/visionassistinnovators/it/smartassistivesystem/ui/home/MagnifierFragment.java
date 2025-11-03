0f5528cba017c0cf5a0e9e1cff82a1de7faac4ab
dakshrana4095 <thedaksh09@gmail.com>
2025-11-02 22:07:04 -0500

    dded email authentication

--- assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/home/MagnifierFragment.java
+++ assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/home/MagnifierFragment.java
@@ -1,10 +1,10 @@
 /**
  * Course Section: OCA
  * Team Members:
+ * Daksh Rana – N01664095
  * Sarang Prajapati – N01662036
  * Krish Patel – N01666556
  * Kush Patel – N01657387
- * Daksh Rana – N01664095
  */
 package ca.visionassistinnovators.it.smartassistivesystem.ui.home;
 
