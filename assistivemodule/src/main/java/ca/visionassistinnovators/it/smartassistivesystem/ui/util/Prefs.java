a6c80f0d139550156302b7d64e2a49cd56a045d8
KrishPatel6556 <krishptl9898@gmail.com>
2025-11-09 15:05:44 -0500

    minor changes

--- assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/util/Prefs.java
+++ assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/util/Prefs.java
@@ -1,4 +1,12 @@
-package ca.visionassistinnovators.it.smartassistivesystem.ui.util;
+
+/**
+ * Course Section: OCA
+ * Team Members
+ * Sarang Prajapati – N01662036
+ * Krish Patel – N01666556
+ * Kush Patel – N01657387
+ * Daksh Rana – N01664095
+ */package ca.visionassistinnovators.it.smartassistivesystem.ui.util;
 
 import android.content.Context;
 import android.content.SharedPreferences;
