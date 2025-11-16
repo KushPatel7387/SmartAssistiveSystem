66b89e06aafa7f4f71527903187200b38acd79fa
PatelKush7387 <kush26540@gmail.com>
2025-11-15 20:34:42 -0500

    Added comments into ProfileFragment.java

--- assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/profile/ProfileFragment.java
+++ assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/profile/ProfileFragment.java
@@ -1,3 +1,11 @@
+/**
+ * Course Section: OCA
+ * Team Members:
+ * Krish Patel – N01666556
+ * Kush Patel – N01657387
+ * Daksh Rana – N01664095
+ * Sarang Prajapati – N01662036
+ */
 package ca.visionassistinnovators.it.smartassistivesystem.ui.profile;
 
 import android.os.Bundle;
