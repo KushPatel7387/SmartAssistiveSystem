7a4f75d61ca7fdbd7da9b13cd0a812e02815def0
dakshrana4095 <thedaksh09@gmail.com>
2025-11-16 14:36:46 -0500

    Updated SOSFragment: improved UI, emergency actions,

--- assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/home/SosFragment.java
+++ assistivemodule/src/main/java/ca/visionassistinnovators/it/smartassistivesystem/ui/home/SosFragment.java
@@ -1,3 +1,12 @@
+/**
+ * Course Section: OCA
+ * Team Members:
+ * Sarang Prajapati – N01662036
+ * Krish Patel – N01666556
+ * Kush Patel – N01657387
+ * Daksh Rana – N01664095
+
+ */
 package ca.visionassistinnovators.it.smartassistivesystem.ui.home;
 
 import android.Manifest;
